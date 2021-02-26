5099a49f9f9a8306a83b6426117d5ac565a17740
bbttboy <46322472+bbttboy@users.noreply.github.com>
2021-02-26 22:09:12 +0800

    Update composition_models.py

--- composition_models.py
+++ composition_models.py
@@ -114,5 +114,59 @@ class ComplexProjectionModule(torch.nn.Module):
         )
 
         def forward(self, x):
+            # x --> (img_features, text_features, CONJUGATE)
             x1 = self.image_features([0])
             x2 = self.bert_features(x[1])
+            # default value of CONJUGATE is 1. Only for rotationally symmetric loss value is -1.
+            # which results in the CONJUGATE of text features in the complex space
+            CONJUGATE = x[2]
+            num_samples = x[0].shape[0]
+            CONJUGATE = CONJUGATE[:num_samples]
+            delta = x2 # text as rotation
+            re_delta = torch.cos(delta)
+            im_delta = CONJUGATE * torch.sin(delta)
+
+            re_score = x1 * re_delta
+            im_score = x2 * im_delta
+
+            concat_x = torch.cat([re_score, im_score], 1)
+            # squeeze --> 压缩; 
+            # unsqueeze(d) --> d是扩充哪一维
+            # 例如: a.shape=(2,3)  a.unsqueeze(-1) --> a.shape=(2, 3, 1)
+            x0copy = x[0].unsqueeze(1)
+            x1 = x1.unsqueeze(1)
+            x2 = x2.unsqueeze(1)
+            re_score = re_score.unsqueeze(1)
+            im_score = im_score.unsqueeze(1)
+
+            return concat_x, x1, x2, x0copy, re_score, im_score
+
+
+class LinearMapping(torch.nn.Module):
+    """
+    This is linear mapping to image space. rho(.)
+    """
+
+    def __init__(self, image_embed_dim=512):
+        super().__init__()
+        self.mapping = torch.nn.Sequential(
+            torch.nn.BatchNorm1d(2 * image_embed_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(2 * image_embed_dim, image_embed_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(image_embed_dim, image_embed_dim),
+        )
+
+    def forward(self, x):
+        theta_linear = self.mapping(x[0])
+        return theta_linear
+
+
+class ConvMapping(torch.nn.Module):
+    """
+    This is convoultional mapping to image space. rho_conv(.)
+    """
+
+    def __init__(self, image_embed_dim=512):
+        super().__init__()
+        
