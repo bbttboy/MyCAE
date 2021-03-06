60922d4ac2f7b05939046005deac4e86a7bf7f80
bbttboy <46322472+bbttboy@users.noreply.github.com>
2021-03-06 18:54:58 +0800

    Create test_retrieval.py

---
+++ test_retrieval.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+import random
+from tqdm import tqdm 
+from collections import OrderedDict
+
+def fiq_test(opt, model, testset):
+    # eval()一般用作测试集和验证集，即他再不会改变model的权重
+    model.eval()
+
+    all_imgs = []
+    all_queries = []
+    all_target_captions = []
+    all_target_ids = []
+
+    imgs = []
+    mods = []
+    out = []
+
+    for i in tqdm(range(len(testset))):
+        torch.cuda.empty_cache()
+        item = testset[i]
+        imgs += [testset.get_img(item['source_img_id'])]
