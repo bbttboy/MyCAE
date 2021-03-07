f944e7a062103550695a8b5dc609a8a3c6917b28
bbttboy <46322472+bbttboy@users.noreply.github.com>
2021-03-07 21:01:00 +0800

    Update test_retrieval.py

--- test_retrieval.py
+++ test_retrieval.py
@@ -4,20 +4,24 @@ import random
 from tqdm import tqdm 
 from collections import OrderedDict
 
-def fiq_test(opt, model, testset):
-    # eval()一般用作测试集和验证集，即他再不会改变model的权重
-    model.eval()
+# def fiq_test(opt, model, testset):
+#     # eval()一般用作测试集和验证集，即他再不会改变model的权重
+#     model.eval()
 
-    all_imgs = []
-    all_queries = []
-    all_target_captions = []
-    all_target_ids = []
+#     all_imgs = []
+#     all_queries = []
+#     all_target_captions = []
+#     all_target_ids = []
 
-    imgs = []
-    mods = []
-    out = []
+#     imgs = []
+#     mods = []
+#     out = []
 
-    for i in tqdm(range(len(testset))):
-        torch.cuda.empty_cache()
-        item = testset[i]
-        imgs += [testset.get_img(item['source_img_id'])]
+#     for i in tqdm(range(len(testset))):
+#         torch.cuda.empty_cache()
+#         item = testset[i]
+#         imgs += [testset.get_img(item['source_img_id'])]
+
+
+def test(opt, model, testset):
+    pass
