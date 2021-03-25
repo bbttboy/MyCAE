4043af636c7e7902ad5471e7c5aed8cc1fab3660
bbttboy <46322472+bbttboy@users.noreply.github.com>
2021-03-25 21:09:26 +0800

    Create bert_dic.py

---
+++ bert_dic.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+class MyBertDic():
+    def __init__(self):
+        self.data = pd.read_csv(r'D:\DataSet\fashion-200k\label_bert_feature.csv', header=None)
+        self.dic = {}
+        for i in range(len(self.data)):
+            self.dic[self.data.iloc[i][0]] = self.data.iloc[i][1:].to_numpy(dtype='float32')
+
+    def encode(self, text):
+        self.features = []
+        for t in text:
+            self.features.append(self.dic[t])
+        self.features_np = np.stack(self.features)
+        return self.features_np
